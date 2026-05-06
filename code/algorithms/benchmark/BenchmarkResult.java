314e5afc46ce5e055bf03f837f0df05f27aef589
Bich Ngoc Le <bichngocleotaku@gmail.com>
2026-05-06 11:23:56 +0700

    add folder benchmark

---
+++ code/algorithms/benchmark/BenchmarkResult.java
@@ -0,0 +1,26 @@
+package benchmark;
+
+public class BenchmarkResult {
+    private String algorithmName;
+    private long timeNano;
+    private int k;
+
+    public BenchmarkResult(String algorithmName, long timeNano, int k) {
+        this.algorithmName = algorithmName;
+        this.timeNano = timeNano;
+        this.k = k;
+    }
+
+    public String getAlgorithmName() {
+        return algorithmName;
+    }
+
+    public long getTimeNano() {
+        return timeNano;
+    }
+
+    @Override
+    public String toString() {
+        return algorithmName + " | avgTime(ns): " + timeNano;
+    }
+}
